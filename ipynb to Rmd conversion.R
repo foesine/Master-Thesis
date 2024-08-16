047d578449de6a1c715420bd4693c20768b83297
foesine <emmafoessing@gmail.com>
2024-08-16 11:26:27 +0200

    Rmds for SDGs and some BN work that failed

--- ipynb to Rmd conversion.R
+++ ipynb to Rmd conversion.R
@@ -3,5 +3,5 @@ library(rmarkdown)
 library(knitr)
 library(xfun)
 
-input <- "/Users/emmafoessing/Documents/Master/MA/Code/Master-Thesis/MLP_prediction_model.ipynb"
+input <- "/Users/emmafoessing/Documents/Master/MA/Code/Master-Thesis/SDG_RF_v2.ipynb"
 convert_ipynb(input, output = xfun::with_ext(input, "Rmd"))
